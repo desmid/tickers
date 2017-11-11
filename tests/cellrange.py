1a2b61407d4cc396d1334852eedbe1cb823451da
nige <nigebrown@gmail.com>
2017-11-11 18:16:55 +0100

    move global funcs into calss as methods

--- tests/cellrange.py
+++ tests/cellrange.py
@@ -2,6 +2,8 @@
 import re
 
 class cellrange(object):
+    ORD_A = ord('A')
+
     def __init__(self, name=''):
         self.set_by_name(name)
 
@@ -15,12 +17,7 @@ class cellrange(object):
 
     def set_by_name(self, name):
         ((self.start_col, self.start_row), (self.end_col, self.end_row)) \
-            = range2posn(name)
-
-    def name2posn(self, name): return name2posn(name)
-    def posn2name(self, pos): return posn2name(pos)
-
-    def range2posn(self, name): return range2posn(name)
+            = self.range2posn(name)
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
@@ -30,126 +27,119 @@ class cellrange(object):
     def __ne__(self, other):
         return not self.__eq__(other)
 
-###########################################################################
-# static
-###########################################################################
-ORD_A = ord('A')
+    def name2posn(self, n=''):
+        """Convert spreadsheet cell names ('A1', AZ2', etc.) and return a
+        pair of 0-based positions as a tuple: (column, row).
 
-def name2posn(n=''):
-    """Convert spreadsheet cell names ('A1', AZ2', etc.) and return a
-    pair of 0-based positions as a tuple: (column, row).
+        - Ignores $ signs.
+        - Harmlessly allows row 0 (treats as row 1).
 
-    - Ignores $ signs.
-    - Harmlessly allows row 0 (treats as row 1).
+        Example usage and return values:
 
-    Example usage and return values:
+        name2posn('')     =>  (0,0)
+        name2posn('0')    =>  (0,0)
+        name2posn('A')    =>  (0,0)
+        name2posn('A0')   =>  (0,0)
+        name2posn('A1')   =>  (0,0)
 
-    name2posn('')     =>  (0,0)
-    name2posn('0')    =>  (0,0)
-    name2posn('A')    =>  (0,0)
-    name2posn('A0')   =>  (0,0)
-    name2posn('A1')   =>  (0,0)
+        name2posn('B')    =>  (1,0)
+        name2posn('B0')   =>  (1,0)
+        name2posn('B1')   =>  (1,0)
+        name2posn('B2')   =>  (1,1)
+        name2posn('Z')    =>  (25,0)
+        name2posn('AA')   =>  (26,0)
+        name2posn('AZ')   =>  (51,0)
+        name2posn('BA')   =>  (52,0)
+        name2posn('ZZ')   =>  (701,0)
+        name2posn('AAA')  =>  (702,0)
 
-    name2posn('B')    =>  (1,0)
-    name2posn('B0')   =>  (1,0)
-    name2posn('B1')   =>  (1,0)
-    name2posn('B2')   =>  (1,1)
-    name2posn('Z')    =>  (25,0)
-    name2posn('AA')   =>  (26,0)
-    name2posn('AZ')   =>  (51,0)
-    name2posn('BA')   =>  (52,0)
-    name2posn('ZZ')   =>  (701,0)
-    name2posn('AAA')  =>  (702,0)
+        """
+        if isinstance(n, int):
+            if n < 0: n = 0
+            return (n, n)
+        if not isinstance(n, str):
+            return (0, 0)
+        n = re.sub('[^A-Z0-9:]', '', str(n).upper())
+        #0-based arithmetic
+        if n is '': n = '1'
+        c, r = 0, 0
+        while len(n) > 0:
+            v = ord(n[0])
+            if v < self.ORD_A:
+                r = int(n)
+                break
+            c = 26*c + v-self.ORD_A+1
+            n = n[1:]
+        if c > 0: c -= 1
+        if r > 0: r -= 1
+        return (c, r)
 
-    """
-    if isinstance(n, int):
-        if n < 0: n = 0
-        return (n, n)
-    if not isinstance(n, str):
-        return (0, 0)
-    n = re.sub('[^A-Z0-9:]', '', str(n).upper())
-    #0-based arithmetic
-    if n is '': n = '1'
-    c, r = 0, 0
-    while len(n) > 0:
-        v = ord(n[0])
-        if v < ORD_A:
-            r = int(n)
-            break
-        c = 26*c + v-ORD_A+1
-        n = n[1:]
-    if c > 0: c -= 1
-    if r > 0: r -= 1
-    return (c, r)
+    def range2posn(self, n=''):
+        """Convert spreadsheet cell name ranges ('A1:A10', B10:G100', etc.)
+        and return a pair of pairs of 0-based positions as a tuple:
+        ((start_column, start_row), (end_column, end_row)).
 
-def posn2name(p):
-    """Convert spreadsheet cell positions as a pair (column, row) and
-    return a cell name as a string: 'A1'
+        - Ignores $ signs.
+        - Harmlessly allows row 0 (treats as row 1).
 
-    Example usage and return values:
- 
-    posn2name((0,0))    =>  'A1'
-    posn2name((0,1))    =>  'A2'
-    posn2name((1,0))    =>  'B1'
-    posn2name((1,1))    =>  'B2'
+        Example usage and return values:
 
-    posn2name((25,0))   =>  'Z1'
-    posn2name((26,0))   =>  'AA1'
-    posn2name((27,0))   =>  'AB1'
+        range2posn()          =>  ((0,0), (0,0))
+        range2posn('')        =>  ((0,0), (0,0))
+        range2posn('A:Z')     =>  ((0,0), (25,0))
+        range2posn('A1:A99')  =>  ((0,0), (0,98))
+        range2posn('A1:C3')   =>  ((0,0), (2,2))
 
-    posn2name((51,0))   =>  'AZ1'
-    posn2name((52,0))   =>  'BA1'
-    posn2name((53,0))   =>  'BB1'
+        """
+        if isinstance(n, int) or not isinstance(n, str):
+            n = self.name2posn(n)
+            return (n, n)
+        try:
+            c,r = n.split(':', 1)
+        except:
+            c = n.split(':')[0]
+            r = c
+        if c == '': c = '0'
+        if r == '': r = '0'
+        return (self.name2posn(c), self.name2posn(r))
 
-    posn2name((701,0))  =>  'ZZ1'
-    posn2name((702,0))  =>  'AAA1'
-    posn2name((703,0))  =>  'AAB1'
-    """
-    try:
-        col,row = p
-    except:
-        raise TypeError("cell positions must an int pair")
-    if not (isinstance(col, int) and isinstance(row, int)):
-        raise TypeError("cell positions must an int pair")
-    if col < 0 or row < 0:
-        raise TypeError("cell positions cannot be negative")
-    col += 1
-    name = ""
-    while col > 0:
-        col, remainder = divmod(col-1, 26)
-        name = chr(ORD_A + remainder) + name
-    return name + str(row+1)
 
-def range2posn(n=''):
-    """Convert spreadsheet cell name ranges ('A1:A10', B10:G100', etc.)
-    and return a pair of pairs of 0-based positions as a tuple:
-    ((start_column, start_row), (end_column, end_row)).
+    def posn2name(self, p):
+        """Convert spreadsheet cell positions as a pair (column, row) and
+        return a cell name as a string: 'A1'
 
-    - Ignores $ signs.
-    - Harmlessly allows row 0 (treats as row 1).
+        Example usage and return values:
+     
+        posn2name((0,0))    =>  'A1'
+        posn2name((0,1))    =>  'A2'
+        posn2name((1,0))    =>  'B1'
+        posn2name((1,1))    =>  'B2'
 
-    Example usage and return values:
+        posn2name((25,0))   =>  'Z1'
+        posn2name((26,0))   =>  'AA1'
+        posn2name((27,0))   =>  'AB1'
 
-    range2posn()          =>  ((0,0), (0,0))
-    range2posn('')        =>  ((0,0), (0,0))
-    range2posn('A:Z')     =>  ((0,0), (25,0))
-    range2posn('A1:A99')  =>  ((0,0), (0,98))
-    range2posn('A1:C3')   =>  ((0,0), (2,2))
+        posn2name((51,0))   =>  'AZ1'
+        posn2name((52,0))   =>  'BA1'
+        posn2name((53,0))   =>  'BB1'
 
-    """
-    if isinstance(n, int):
-        n = name2posn(n)
-        return (n, n)
-    if not isinstance(n, str):
-        n = name2posn(n)
-        return (n, n)
-    try:
-        c,r = n.split(':', 1)
-    except:
-        c = n.split(':')[0]
-        r = c
-    if c == '': c = '0'
-    if r == '': r = '0'
-    return (name2posn(c), name2posn(r))
+        posn2name((701,0))  =>  'ZZ1'
+        posn2name((702,0))  =>  'AAA1'
+        posn2name((703,0))  =>  'AAB1'
+        """
+        try:
+            col,row = p
+        except:
+            raise TypeError("cell positions must an int pair")
+        if not (isinstance(col, int) and isinstance(row, int)):
+            raise TypeError("cell positions must an int pair")
+        if col < 0 or row < 0:
+            raise TypeError("cell positions cannot be negative")
+        col += 1
+        name = ""
+        while col > 0:
+            col, remainder = divmod(col-1, 26)
+            name = chr(self.ORD_A + remainder) + name
+        return name + str(row+1)
 
 ###########################################################################
--- tests/test_cellrange.py
+++ tests/test_cellrange.py
@@ -1,128 +1,6 @@
 import unittest
 
-from cellrange import cellrange, name2posn, range2posn, posn2name
-
-class test_name2posn(unittest.TestCase):
-    def test_name2posn_with_empty(self):
-        self.assertEqual(name2posn(), (0,0))
-        self.assertEqual(name2posn(None), (0,0))
-        self.assertEqual(name2posn(False), (0,0))
-        self.assertEqual(name2posn(''), (0,0))
-        self.assertEqual(name2posn(' '), (0,0))
-
-    def test_name2posn_with_int(self):
-        self.assertEqual(name2posn(0), (0,0))
-        self.assertEqual(name2posn(1), (1,1))
-        self.assertEqual(name2posn(2), (2,2))
-        self.assertEqual(name2posn(-2), (0,0))
-
-    def test_name2posn_dollars(self):
-        self.assertEqual(name2posn(' $	'), (0,0))
-        self.assertEqual(name2posn('$0'), (0,0))
-        self.assertEqual(name2posn('$A'), (0,0))
-        self.assertEqual(name2posn('$A$0'), (0,0))
-        self.assertEqual(name2posn('$A$1'), (0,0))
-
-    def test_name2posn_A1_forgiving(self):
-        self.assertEqual(name2posn('0'), (0,0))
-        self.assertEqual(name2posn('00'), (0,0))
-        self.assertEqual(name2posn('A0'), (0,0))
-        self.assertEqual(name2posn('01'), (0,0))
-        self.assertEqual(name2posn('02'), (0,1))
-
-    def test_name2posn_A(self):
-        self.assertEqual(name2posn('A'), (0,0))
-        self.assertEqual(name2posn('A1'), (0,0))
-        self.assertEqual(name2posn('A2'), (0,1))
-
-    def test_name2posn_B(self):
-        self.assertEqual(name2posn('B'), (1,0))
-        self.assertEqual(name2posn('B1'), (1,0))
-        self.assertEqual(name2posn('B2'), (1,1))
-
-    #boundaries 1 to 2 chars
-    def test_name2posn_Z1(self):
-        self.assertEqual(name2posn('Z'), (25,0))
-    def test_name2posn_AA(self):
-        self.assertEqual(name2posn('AA'), (26,0))
-    def test_name2posn_AB(self):
-        self.assertEqual(name2posn('AB'), (27,0))
-
-    #boundaries within 2 chars
-    def test_name2posn_AZ(self):
-        self.assertEqual(name2posn('AZ'), (51,0))
-    def test_name2posn_BA(self):
-        self.assertEqual(name2posn('BA'), (52,0))
-
-    #boundaries 2 to 3 chars
-    def test_name2posn_ZZ(self):
-        self.assertEqual(name2posn('ZZ'), (701,0))
-    def test_name2posn_AAA(self):
-        self.assertEqual(name2posn('AAA'), (702,0))
-
-###########################################################################
-class test_posn2name(unittest.TestCase):
-    def test_posn2name_with_empty(self):
-        with self.assertRaises(TypeError):
-            posn2name()
-        with self.assertRaises(TypeError):
-            posn2name(False)
-        with self.assertRaises(TypeError):
-            posn2name(None)
-        with self.assertRaises(TypeError):
-            posn2name('')
-        with self.assertRaises(TypeError):
-            posn2name(0)
-
-    def test_posn2name_with_not_int(self):
-        with self.assertRaises(TypeError):
-            posn2name( ('a',1) )
-        with self.assertRaises(TypeError):
-            posn2name( (1,'a') )
-        
-    def test_posn2name_with_negative(self):
-        with self.assertRaises(TypeError):
-            posn2name( (-1,0) )
-        with self.assertRaises(TypeError):
-            posn2name( (0,-1) )
-        
-    def test_posn2name_with_pair_A_to_Z(self):
-        self.assertEqual(posn2name((0,0)), 'A1')
-        self.assertEqual(posn2name((0,1)), 'A2')
-        self.assertEqual(posn2name((1,0)), 'B1')
-        self.assertEqual(posn2name((1,1)), 'B2')
-
-    def test_posn2name_with_pair_Z_to_AA_AB(self):
-        self.assertEqual(posn2name((25,0)), 'Z1')
-        self.assertEqual(posn2name((26,0)), 'AA1')
-        self.assertEqual(posn2name((27,0)), 'AB1')
-
-    def test_posn2name_with_pair_AZ_to_AA_to_AB(self):
-        self.assertEqual(posn2name((51,0)), 'AZ1')
-        self.assertEqual(posn2name((52,0)), 'BA1')
-        self.assertEqual(posn2name((53,0)), 'BB1')
-
-    def test_posn2name_with_pair_ZZ_to_AAA_to_AAB(self):
-        self.assertEqual(posn2name((701,0)), 'ZZ1')
-        self.assertEqual(posn2name((702,0)), 'AAA1')
-        self.assertEqual(posn2name((703,0)), 'AAB1')
-
-###########################################################################
-class test_range2posn(unittest.TestCase):
-    def test_range2posn_with_empty(self):
-        self.assertEqual(range2posn(), ((0,0), (0,0)))
-        self.assertEqual(range2posn(None), ((0,0), (0,0)))
-        self.assertEqual(range2posn(False), ((0,0), (0,0)))
-        self.assertEqual(range2posn(''), ((0,0), (0,0)))
-
-    def test_range2posn_with_A_Z(self):
-        self.assertEqual(range2posn('A:Z'), ((0,0), (25,0)))
-
-    def test_range2posn_with_A1_A99(self):
-        self.assertEqual(range2posn('A1:A99'), ((0,0), (0,98)))
-
-    def test_range2posn_with_A1_C3(self):
-        self.assertEqual(range2posn('A1:C3'), ((0,0), (2,2)))
+from cellrange import cellrange
 
 ###########################################################################
 class test_cellrange(unittest.TestCase):
@@ -204,17 +82,135 @@ class test_cellrange_compare(unittest.TestCase):
         self.assertFalse(c == d)
         self.assertTrue(c != d)
 
-class test_cellrange_static_wrappers(unittest.TestCase):
-    def test_name2posn_B2(self):
-        c = cellrange()
-        self.assertEqual(c.name2posn('B2'), (1,1))
+class test_cellrange_name2posn(unittest.TestCase):
+    def setUp(self):
+        self.c = cellrange()
 
-    def test_posn2name_B2(self):
-        c = cellrange()
-        self.assertEqual(c.posn2name((1,1)), 'B2')
+    def test_name2posn_with_empty(self):
+        self.assertEqual(self.c.name2posn(), (0,0))
+        self.assertEqual(self.c.name2posn(None), (0,0))
+        self.assertEqual(self.c.name2posn(False), (0,0))
+        self.assertEqual(self.c.name2posn(''), (0,0))
+        self.assertEqual(self.c.name2posn(' '), (0,0))
+
+    def test_name2posn_with_int(self):
+        self.assertEqual(self.c.name2posn(0), (0,0))
+        self.assertEqual(self.c.name2posn(1), (1,1))
+        self.assertEqual(self.c.name2posn(2), (2,2))
+        self.assertEqual(self.c.name2posn(-2), (0,0))
+
+    def test_name2posn_dollars(self):
+        self.assertEqual(self.c.name2posn(' $	'), (0,0))
+        self.assertEqual(self.c.name2posn('$0'), (0,0))
+        self.assertEqual(self.c.name2posn('$A'), (0,0))
+        self.assertEqual(self.c.name2posn('$A$0'), (0,0))
+        self.assertEqual(self.c.name2posn('$A$1'), (0,0))
+
+    def test_name2posn_A1_forgiving(self):
+        self.assertEqual(self.c.name2posn('0'), (0,0))
+        self.assertEqual(self.c.name2posn('00'), (0,0))
+        self.assertEqual(self.c.name2posn('A0'), (0,0))
+        self.assertEqual(self.c.name2posn('01'), (0,0))
+        self.assertEqual(self.c.name2posn('02'), (0,1))
+
+    def test_name2posn_A(self):
+        self.assertEqual(self.c.name2posn('A'), (0,0))
+        self.assertEqual(self.c.name2posn('A1'), (0,0))
+        self.assertEqual(self.c.name2posn('A2'), (0,1))
+
+    def test_name2posn_B(self):
+        self.assertEqual(self.c.name2posn('B'), (1,0))
+        self.assertEqual(self.c.name2posn('B1'), (1,0))
+        self.assertEqual(self.c.name2posn('B2'), (1,1))
+
+    #boundaries 1 to 2 self.c.ars
+    def test_name2posn_Z1(self):
+        self.assertEqual(self.c.name2posn('Z'), (25,0))
+    def test_name2posn_AA(self):
+        self.assertEqual(self.c.name2posn('AA'), (26,0))
+    def test_name2posn_AB(self):
+        self.assertEqual(self.c.name2posn('AB'), (27,0))
+
+    #boundaries within 2 self.c.ars
+    def test_name2posn_AZ(self):
+        self.assertEqual(self.c.name2posn('AZ'), (51,0))
+    def test_name2posn_BA(self):
+        self.assertEqual(self.c.name2posn('BA'), (52,0))
+
+    #boundaries 2 to 3 self.c.ars
+    def test_name2posn_ZZ(self):
+        self.assertEqual(self.c.name2posn('ZZ'), (701,0))
+    def test_name2posn_AAA(self):
+        self.assertEqual(self.c.name2posn('AAA'), (702,0))
+
+###########################################################################
+class test_range2posn(unittest.TestCase):
+    def setUp(self):
+        self.c = cellrange()
+
+    def test_range2posn_with_empty(self):
+        self.assertEqual(self.c.range2posn(), ((0,0), (0,0)))
+        self.assertEqual(self.c.range2posn(None), ((0,0), (0,0)))
+        self.assertEqual(self.c.range2posn(False), ((0,0), (0,0)))
+        self.assertEqual(self.c.range2posn(''), ((0,0), (0,0)))
+
+    def test_range2posn_with_A_Z(self):
+        self.assertEqual(self.c.range2posn('A:Z'), ((0,0), (25,0)))
 
     def test_range2posn_with_A1_A99(self):
-        c = cellrange()
-        self.assertEqual(c.range2posn('A1:A99'), ((0,0), (0,98)))
+        self.assertEqual(self.c.range2posn('A1:A99'), ((0,0), (0,98)))
+
+    def test_range2posn_with_A1_C3(self):
+        self.assertEqual(self.c.range2posn('A1:C3'), ((0,0), (2,2)))
+
+###########################################################################
+class test_posn2name(unittest.TestCase):
+    def setUp(self):
+        self.c = cellrange()
+
+    def test_posn2name_with_empty(self):
+        with self.assertRaises(TypeError):
+            self.c.posn2name()
+        with self.assertRaises(TypeError):
+            self.c.posn2name(False)
+        with self.assertRaises(TypeError):
+            self.c.posn2name(None)
+        with self.assertRaises(TypeError):
+            self.c.posn2name('')
+        with self.assertRaises(TypeError):
+            self.c.posn2name(0)
+
+    def test_posn2name_with_not_int(self):
+        with self.assertRaises(TypeError):
+            self.c.posn2name( ('a',1) )
+        with self.assertRaises(TypeError):
+            self.c.posn2name( (1,'a') )
+        
+    def test_posn2name_with_negative(self):
+        with self.assertRaises(TypeError):
+            self.c.posn2name( (-1,0) )
+        with self.assertRaises(TypeError):
+            self.c.posn2name( (0,-1) )
+        
+    def test_posn2name_with_pair_A_to_Z(self):
+        self.assertEqual(self.c.posn2name((0,0)), 'A1')
+        self.assertEqual(self.c.posn2name((0,1)), 'A2')
+        self.assertEqual(self.c.posn2name((1,0)), 'B1')
+        self.assertEqual(self.c.posn2name((1,1)), 'B2')
+
+    def test_posn2name_with_pair_Z_to_AA_AB(self):
+        self.assertEqual(self.c.posn2name((25,0)), 'Z1')
+        self.assertEqual(self.c.posn2name((26,0)), 'AA1')
+        self.assertEqual(self.c.posn2name((27,0)), 'AB1')
+
+    def test_posn2name_with_pair_AZ_to_AA_to_AB(self):
+        self.assertEqual(self.c.posn2name((51,0)), 'AZ1')
+        self.assertEqual(self.c.posn2name((52,0)), 'BA1')
+        self.assertEqual(self.c.posn2name((53,0)), 'BB1')
+
+    def test_posn2name_with_pair_ZZ_to_AAA_to_AAB(self):
+        self.assertEqual(self.c.posn2name((701,0)), 'ZZ1')
+        self.assertEqual(self.c.posn2name((702,0)), 'AAA1')
+        self.assertEqual(self.c.posn2name((703,0)), 'AAB1')
 
 ###########################################################################
